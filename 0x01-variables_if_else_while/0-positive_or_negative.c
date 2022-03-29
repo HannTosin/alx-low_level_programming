9932dee41b69eec85d28d7f283e0bd0a525c6105
@HannTosin <abrahamtosin7@gmail.com>
2022-03-29 06:39:08 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 
+ |
+
 /..
  *main - Entry point
  *
@@ -27,7 +29,7 @@ printf("%d is negative\n", n) :
 
 else
 {
-printf("% d is positive\n",  \n) :
+printf("% d is positive\n", n) :
 }
 
 return (0) :
